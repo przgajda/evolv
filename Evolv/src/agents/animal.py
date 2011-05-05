93b46ae1f9e7266100839bc92741289e07f5b32f
quermit <quermit@cc734b01-7559-1909-3089-11d117e361b5>
2011-05-05 14:48:15 +0000

    genetics

--- Evolv/src/agents/animal.py
+++ Evolv/src/agents/animal.py
@@ -3,3 +3,26 @@ Created on 2011-05-05
 
 @author: quermit
 '''
+from breve import Mobile
+
+
+class Animal(Mobile):
+
+    def __init__(self):
+        super(Animal, self).__init__()
+
+        self.energy = 0
+        self.health = 0
+        self.age = 0
+
+
+class Rabbit(Animal):
+
+    def __init__(self):
+        super(Rabbit, self).__init__()
+
+
+class Wolf(Animal):
+
+    def __init__(self):
+        super(Rabbit, self).__init__()
